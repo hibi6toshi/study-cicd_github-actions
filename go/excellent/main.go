5834f435c4c15a1f24d9e874cc02092dbe1e4291
Hidetoshi Hibino <hibinozzzz@gmail.com>
2025-04-13 03:13:40 +0900

    fix go file directory

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
-var version string // ビルド時にldflagsフラグ経由でバージョンを埋め込むための変数
-
-func main() {
-	fmt.Printf("Example %s\n", version)
-}
-
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "even"
