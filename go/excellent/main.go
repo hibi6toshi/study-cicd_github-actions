838b4d30c0ec0b445ec3b6c5c73425522dfaf9f3
Hidetoshi Hibino <hibinozzzz@gmail.com>
2025-04-13 02:15:18 +0900

    add main func to main.go

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
+var version string // ビルド時にldflagsフラグ経由でバージョンを埋め込むための変数
+
+func main() {
+	fmt.Printf("Example %s\n", version)
+}
+
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "even"
@@ -7,3 +15,5 @@ func EvenOrOdd(number int) string {
 		return "odd"
 	}
 }
+
+
